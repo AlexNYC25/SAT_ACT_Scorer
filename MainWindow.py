3d8ff77554dde38fb72014f99bc2f9be557fe711
Alexis Montes <montes.alexis25@gmail.com>
2019-09-29 01:07:54 -0400

    Initital commit

---
+++ MainWindow.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+# TODO: fix layout of buttons to center them more in the center, assign function to button
+
+class MainWindow():
+
+    def __init__(self):
+        root = Tk()
+        root.geometry("500x200")
+        root.title("SAT and ACT score guide")
+
+        greeting = Label(root, text="Hello User")
+        greeting.pack( side="top")
+
+        choice = Label(root, text="Which test do you need help scoring today")
+        choice.pack(side="top")
+
+        satButton = Button(root, text="SAT", width="10", height="5", padx="5")
+        actButton = Button(root, text="ACT", width="10", height="5", padx="5")
+
+        satButton.pack(side="left")
+        actButton.pack(side="right")
+        root.mainloop()
+
+
+def main():
+    initial = MainWindow()
+
+if __name__ == "__main__":
+    main()
