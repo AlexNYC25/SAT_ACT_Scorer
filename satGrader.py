a43ecb4210af6f0eb95baeeed41e6160ff013931
Alexis Montes <montes.alexis25@gmail.com>
2019-10-13 23:43:31 -0400

    continued on json implementaion

--- satGrader.py
+++ satGrader.py
@@ -3,21 +3,44 @@ import json
 class satGrader:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.rawMathScore = None
-        self.rawWritingScore = None
-        self.rawReadingScore = None
+        self.finalMathScore = None
+        self.finalWritingScore = None
+        self.finalReadingScore = None
         self.finalTotalScore = None
 
-    def calculateMathScore(self):
+    def calculateMathScore(self, score, category):
+        # error handling
+
+        # if score is not number
+            #if(int(score))
+        try:
+            foo = int(score)
+        except ValueError as err:
+            print(err)
+            return ValueError
+
+        if(category != 'Math' or category != 'Reading' or category != "Writing"):
+            return ValueError
+
+        # temp[score].get[Str(score)]
         with open('JSON/satScoreGuide1.json', 'r') as jsonFile:
             data = json.load(jsonFile)
-            temp = data['Math']
+            temp = data[category]
 
+            # if categories for right final score var
+
+
+            print(temp[score].get(str(score)))
+
+            '''
+            Original alg
             for x in temp:
-                print(x)
                 for (key) in x:
-                    print(x.get(key))
+                    if(key == '7'):
+                        print("found")
+                        print(x.get(key))
+            '''
 
 
 test = satGrader()
-test.calculateMathScore()
+test.calculateMathScore("7m", 'Math')
